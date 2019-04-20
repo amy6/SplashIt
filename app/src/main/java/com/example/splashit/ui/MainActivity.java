0f7b7e03f4f20f9ce1faa9ef0f7c53503fc3971b
Mahima <mahima.6.ms@gmail.com>
2019-04-20 12:58:06 +0530

    rearrange files into packages

--- app/src/main/java/com/example/splashit/ui/MainActivity.java
+++ app/src/main/java/com/example/splashit/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.splashit;
+package com.example.splashit.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.splashit.R;
 
 public class MainActivity extends AppCompatActivity {
 
