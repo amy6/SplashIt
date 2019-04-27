151ca58830479033c6cd94fa1a549e5819adef97
Mahima <mahima.6.ms@gmail.com>
2019-04-27 21:23:53 +0530

    set up grid widget for home screen

--- app/src/main/java/com/example/splashit/PhotoAppWidget.java
+++ app/src/main/java/com/example/splashit/PhotoAppWidget.java
@@ -1,9 +1,14 @@
 package com.example.splashit;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.splashit.ui.PhotoDetailActivity;
+import com.example.splashit.widget.GridWidgetService;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,31 +18,25 @@ public class PhotoAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.photo_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
-        // Instruct the widget manager to update the widget
+        Intent intent = new Intent(context, GridWidgetService.class);
+        views.setRemoteAdapter(R.id.widget_grid_view, intent);
+
+        Intent appIntent = new Intent(context, PhotoDetailActivity.class);
+        PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.widget_grid_view, appPendingIntent);
+
+        views.setEmptyView(R.id.widget_grid_view, R.id.empty_view);
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
--- app/src/main/java/com/example/splashit/widget/GridWidgetService.java
+++ app/src/main/java/com/example/splashit/widget/GridWidgetService.java
@@ -1,36 +1,18 @@
 package com.example.splashit.widget;
 
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
-import com.example.android.mygarden.provider.PlantContract;
-import com.example.android.mygarden.ui.PlantDetailActivity;
-import com.example.android.mygarden.utils.PlantUtils;
+import com.example.splashit.R;
+import com.example.splashit.data.database.PhotoDatabase;
+import com.example.splashit.data.model.Photo;
 
-import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
-import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GridWidgetService extends RemoteViewsService {
     @Override
@@ -41,84 +23,50 @@ public class GridWidgetService extends RemoteViewsService {
 
 class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
-    Context mContext;
-    Cursor mCursor;
+    private static final String TAG = GridRemoteViewsFactory.class.getSimpleName();
+
+    private Context context;
+    private List<Photo> favoritesPhotos;
+    private PhotoDatabase db;
 
     public GridRemoteViewsFactory(Context applicationContext) {
-        mContext = applicationContext;
-
+        context = applicationContext;
     }
 
     @Override
     public void onCreate() {
-
+        favoritesPhotos = new ArrayList<>();
+        Log.i(TAG, "Initializing favorites photos array list : " + favoritesPhotos.size());
+        db = PhotoDatabase.getInstance(context);
     }
 
-    //called on start and when notifyAppWidgetViewDataChanged is called
     @Override
     public void onDataSetChanged() {
-        // Get all plant info ordered by creation time
-        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
-        if (mCursor != null) mCursor.close();
-        mCursor = mContext.getContentResolver().query(
-                PLANT_URI,
-                null,
-                null,
-                null,
-                PlantContract.PlantEntry.COLUMN_CREATION_TIME
-        );
+        List<Photo> photoList = db.photoDao().getFavoritesPhotos().getValue();
+        Log.i(TAG, "Favorites photos inside onDataSetChanged after DB call : " + favoritesPhotos.size());
+        if (photoList != null) {
+            favoritesPhotos.addAll(photoList);
+            Log.i(TAG, "Favorites photos inside onDataSetChanged after adding to list : " + favoritesPhotos.size());
+        }
     }
 
     @Override
     public void onDestroy() {
-        mCursor.close();
+        db.close();
     }
 
     @Override
     public int getCount() {
-        if (mCursor == null) return 0;
-        return mCursor.getCount();
+        if (favoritesPhotos == null) return 0;
+        Log.i(TAG, "Favorites photos : " + favoritesPhotos.size());
+        return favoritesPhotos.size();
     }
 
-    /**
-     * This method acts like the onBindViewHolder method in an Adapter
-     *
-     * @param position The current position of the item in the GridView to be displayed
-     * @return The RemoteViews object to display for the provided postion
-     */
     @Override
     public RemoteViews getViewAt(int position) {
-        if (mCursor == null || mCursor.getCount() == 0) return null;
-        mCursor.moveToPosition(position);
-        int idIndex = mCursor.getColumnIndex(PlantContract.PlantEntry._ID);
-        int createTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
-        int waterTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
-        int plantTypeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
-
-        long plantId = mCursor.getLong(idIndex);
-        int plantType = mCursor.getInt(plantTypeIndex);
-        long createdAt = mCursor.getLong(createTimeIndex);
-        long wateredAt = mCursor.getLong(waterTimeIndex);
-        long timeNow = System.currentTimeMillis();
-
-        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.plant_widget);
-
-        // Update the plant image
-        int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow - createdAt, timeNow - wateredAt, plantType);
-        views.setImageViewResource(R.id.widget_plant_image, imgRes);
-        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
-        // Always hide the water drop in GridView mode
-        views.setViewVisibility(R.id.widget_water_button, View.GONE);
-
-        // Fill in the onClick PendingIntent Template using the specific plant Id for each item individually
-        Bundle extras = new Bundle();
-        extras.putLong(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
-        Intent fillInIntent = new Intent();
-        fillInIntent.putExtras(extras);
-        views.setOnClickFillInIntent(R.id.widget_plant_image, fillInIntent);
-
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.photo_app_widget_list);
+        views.setImageViewUri(R.id.widget_photo_image, Uri.parse(favoritesPhotos.get(position).getUrls().getRaw()));
         return views;
-
     }
 
     @Override
@@ -128,7 +76,7 @@ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public int getViewTypeCount() {
-        return 1; // Treat all items in the GridView the same
+        return 1;
     }
 
     @Override
