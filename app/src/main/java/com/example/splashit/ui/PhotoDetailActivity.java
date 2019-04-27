14b45211eee0ae741e2f7baa762f5fc94e5dd9ba
Mahima <mahima.6.ms@gmail.com>
2019-04-27 10:40:37 +0530

    add back button and favorite button in detail screen

--- app/src/main/java/com/example/splashit/ui/PhotoDetailActivity.java
+++ app/src/main/java/com/example/splashit/ui/PhotoDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.splashit.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.splashit.R;
 import com.example.splashit.utils.Constants;
@@ -19,5 +21,26 @@ public class PhotoDetailActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().add(R.id.container, PhotoDetailFragment.newInstance(id)).commit();
             }
         }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.photo_detail_menu, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.favorite:
+                // TODO: handle favorite
+                break;
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
